f1384a48df918b2635abd5652e4ce242f3cb042e
AbdullahTgr <4multijob@gmail.com>
2022-01-15 08:15:03 +0200

    change color font

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -6,12 +6,11 @@
 						<div class="col-sm-2">
 							<div class="logo">
 									<img style="
-									width: 80%;
-    padding-top: 10px;
+									width: 70px;
 									" src="{{asset('assets/logo/favicon.png')}}">
 							</div><!-- /.logo-->
 						</div><!-- /.col-->
-						<div class="col-sm-10">
+						<div class="col-sm-10"> 
 							<div class="main-menu">
 							
 								<!-- Brand and toggle get grouped for better mobile display -->
@@ -23,11 +22,26 @@
 								<div class="collapse navbar-collapse">		  
 									<ul class="nav navbar-nav navbar-right">
 
-										<li class="smooth-menu"><a href="#home">Stays</a></li>
-										<li class="smooth-menu"><a href="#gallery">Flights</a></li>
-										<li class="smooth-menu"><a href="#pack">Car  Rental </a></li>
-										<li class="smooth-menu"><a href="#spo">Attractions</a></li>
-										<li class="smooth-menu"><a href="#blog">Airport Taxis</a></li>
+										<li class="smooth-menu"><a href="#home">Homepage</a></li>
+										<li class="smooth-menu"><a href="#gallery">About us</a></li>
+										<li class="smooth-menu"><a href="#gallery">Flight</a></li>
+										<li class="smooth-menu"><a href="#pack">Hotel Booking </a></li>
+										<li class="smooth-menu"><a href="#blog">Services</a></li>
+										<li class="smooth-menu"><a href="#blog">Contact Us</a></li>
+
+
+										<li class="nav-item dropdown">
+											<a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+											  Dropdown
+											</a>
+											<ul class="dropdown-menu dropdown-menu-dark vvd" aria-labelledby="navbarDarkDropdownMenuLink" style="background: rgb(140 140 140 / 88%);">
+												<li class="smooth-menu"><a href="#pack">Packages </a></li>
+												<li class="smooth-menu"><a href="#spo">Daily Tours</a></li>
+												<li class="smooth-menu"><a href="#blog">Outgoing</a></li>
+												<li class="smooth-menu"><a href="#home">Domestic</a></li>
+												<li class="smooth-menu"><a href="#spo">Haj&Omra</a></li>
+											</ul>
+										  </li>
 										
 									</ul>
 								</div><!-- /.navbar-collapse -->
